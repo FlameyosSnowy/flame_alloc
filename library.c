88e87709a69e9510c6c9b82b8549cad8e8fb509d
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2025-06-13 18:04:29 +0300

    Add files via upload

---
+++ library.c
@@ -0,0 +1,89 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#define HEAP_SIZE (1024 * 1024)
+
+#define NULL_PTR 404
+#define FREE_SUCCESS 0
+
+typedef struct AlignedMemoryChunk {
+    uint32_t size;
+    bool in_use;
+    struct AlignedMemoryChunk* next;
+} AlignedMemoryChunk;
+
+typedef struct {
+    uint32_t available;
+    AlignedMemoryChunk* head;
+} AvailableHeap;
+
+void* global_heap_memory = nullptr;
+AvailableHeap heap;
+
+int init_heap() {
+    global_heap_memory = mmap(nullptr, HEAP_SIZE, PROT_READ | PROT_WRITE,
+                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (global_heap_memory == MAP_FAILED) {
+        return 1;
+    }
+
+    heap.available = HEAP_SIZE;
+
+    const auto chunk = (AlignedMemoryChunk*)global_heap_memory;
+    chunk->size = HEAP_SIZE - sizeof(AlignedMemoryChunk);
+    chunk->in_use = false;
+    chunk->next = nullptr;
+
+    heap.head = chunk;
+    return 0;
+}
+
+void* flame_alloc(const uint32_t amount) {
+    AlignedMemoryChunk* curr = heap.head;
+
+    while (curr != nullptr) {
+        if (!curr->in_use && curr->size >= amount) {
+            curr->in_use = true;
+            heap.available -= curr->size;
+            return curr + 1; // return pointer to the data after the header
+        }
+        curr = curr->next;
+    }
+
+    return nullptr;
+}
+
+int flame_free(const void* ptr) {
+    if (ptr == nullptr) return NULL_PTR;
+
+    AlignedMemoryChunk* chunk = (AlignedMemoryChunk*) ptr - 1;
+    chunk->in_use = false;
+    heap.available += chunk->size;
+    return FREE_SUCCESS;
+}
+
+typedef struct {
+    int yo;
+} Something;
+
+int main() {
+    const int status_code = init_heap();
+    if (status_code == 1) {
+        perror("Unable to initialize the heap due to a mmap error. (Likely out of memory)");
+        exit(1);
+    }
+
+    const auto allocated_memory = (Something*) flame_alloc(sizeof(Something));
+    allocated_memory->yo = 50;
+
+    printf("%d\n", allocated_memory->yo); // Prints 50
+
+    allocated_memory->yo = 64;
+    printf("%d\n", allocated_memory->yo); // Prints 64
+    flame_free(allocated_memory);
+
+    return 0;
+}
